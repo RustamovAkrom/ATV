89fadfbef8f931f90d70189c28e1d55801eb96d4
= <akromjonrustamov56@gmail.com>
2026-05-06 00:19:17 +0500

    make best practice in app.py

--- src/tasks/audit_task.py
+++ src/tasks/audit_task.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from core.celery import celery_app
-from core.database.db_async import get_async_session_factory
+from core.database.db_sync import get_sync_session_factory
 from repositories.audit.audit_repo import AuditRepository
 from schemas.audit import AuditCreateSchema
 from services.audit.audit_service import AuditService
@@ -9,14 +9,11 @@ from services.audit.audit_service import AuditService
 
 @celery_app.task(bind=True, max_retries=3)
 def process_audit_log_task(self, payload: dict):
-    async def _run():
-        session_factory = get_async_session_factory()
-        async with session_factory() as session:
-            async with session.begin():
-                service = AuditService(AuditRepository(session))
-                await service.persist_audit(AuditCreateSchema(**payload))
-
     try:
-        asyncio.run(_run())
+        session_factory = get_sync_session_factory()
+        with session_factory() as session:
+            with session.begin():
+                service = AuditService(AuditRepository(session))
+                service.persist_audit(AuditCreateSchema(**payload))
     except Exception as exc:
-        raise self.retry(exc=exc, countdown=5) from exc
+        raise self.retry(exc=exc, countdown=5) from exec
